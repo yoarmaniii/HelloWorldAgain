8887668e99704fcc3d605b9c6372f4f7fb38fa9c
Armani Raleigh <armanijolee@gmail.com>
2016-10-03 21:44:39 -0400

    Testing new IntelliJ IDE

---
+++ src/Runner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Scanner input = new Scanner(System.in);
+        String choice = "yes";
+        String choice2 = "no";
+        input.nextLine();
+
+        if (choice.equals("yes"))
+            System.out.println("It works");
+        {
+            else{
+            System.out.println("it doesnt work man!!");
+        }
+        }
+    }
+}
